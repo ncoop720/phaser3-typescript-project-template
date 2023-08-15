4027c82dcb34ae5ae1b69733d88288cb9a3830e2
unknown <ncoop720@yahoo.com>
2023-08-14 20:16:18 -0400

    move player to click based on world coordinates

--- src/game.ts
+++ src/game.ts
@@ -1,26 +1,36 @@
 import * as Phaser from 'phaser'
 
-let testSprite1
+let player
 let click
 
 function preload() {
+  this.load.image("background", "assets/background.png")
   this.load.image("test1", "assets/test.png")
 }
 
 function create() {
-  testSprite1 = this.physics.add.sprite(50, 50, "test1")
+  const background = this.add.image(0, 0, "background")
+  background.setOrigin(0, 0)
+
+  player = this.physics.add.sprite(500, 500, "test1")
+
+  this.cameras.main.setBounds(0, 0, background.displayWidth, background.displayHeight)
+  this.cameras.main.startFollow(player)
+
   click = new Phaser.Math.Vector2()
 
+  // handle mouse button
   this.input.on('pointerup', pointer => {
-    click.x = pointer.x
-    click.y = pointer.y
-    this.physics.moveToObject(testSprite1, click, 200)
+    click.x = pointer.worldX
+    click.y = pointer.worldY
+    this.physics.moveToObject(player, click, 100)
   })
 }
 
 function update() {
-  if (Phaser.Math.Distance.Between(testSprite1.x, testSprite1.y, click.x, click.y) < 2) {
-    testSprite1.body.reset(click.x, click.y)
+  // stop player if it reaches click destination
+  if (Phaser.Math.Distance.Between(player.x, player.y, click.x, click.y) < 2) {
+    player.body.reset(click.x, click.y)
   }
 }
 
